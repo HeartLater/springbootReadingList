98e29a8c7064263a4ac2e80fb6ab2f94bba15e39
wl <wanglei1201@sohu.com>
2018-12-11 11:10:43 +0800

    修改为modelAndView

--- src/main/java/com/sugon/readinglist/controller/ReadingListController.java
+++ src/main/java/com/sugon/readinglist/controller/ReadingListController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -28,11 +29,20 @@ public class ReadingListController {
         this.readingListRepository = readingListRepository;
     }*/
 
-    @RequestMapping(value = "/{reader}",method = RequestMethod.GET)
+/*    @RequestMapping(value = "/{reader}",method = RequestMethod.GET)
     public String readersBooks(@PathVariable String reader, Model model){
         List<Book> readingList = readingListRepository.findByReader(reader);
         if (readingList != null) {
             model.addAttribute("books",readingList);
+        }
+        return "readingList";
+    }*/
+    @RequestMapping(value = "/{reader}",method = RequestMethod.GET)
+    public String readersBooks(@PathVariable String reader){
+        ModelAndView modelAndView = new ModelAndView(reader);
+        List<Book> readingList = readingListRepository.findByReader(reader);
+        if (readingList != null) {
+            modelAndView.addObject("books",readingList);
         }
         return "readingList";
     }
